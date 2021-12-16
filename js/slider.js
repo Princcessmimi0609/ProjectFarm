6267c8da8bf938be505eee3c26e33e0de2867091
Gazzabwoy <garyduffner@gmail.com>
2021-12-16 12:55:55 +0000

    editing

--- js/slider.js
+++ js/slider.js
@@ -1,123 +1,60 @@
-let i = 0;
- 
-let box1 = document.getElementById("image1");
-let box2 = document.getElementById("image2");
-let box3 = document.getElementById("image3");
-let box4 = document.getElementById("image4");
-let a1 = document.querySelector(".a1");
-let a2 = document.querySelector(".a2");
-let a3 = document.querySelector(".a3");
-let a4 = document.querySelector(".a4");
-let info = document.getElementById("info");
- 
-a1.onclick = function () {
-  box1.style.opacity = 1;
-  box2.style.opacity = 0;
-  box3.style.opacity = 0;
-  box4.style.opacity = 0;
-  info.innerHTML = "Image 1";
-  i = 0;
-};
- 
-a2.onclick = function () {
-  box1.style.opacity = 0;
-  box2.style.opacity = 1;
-  box3.style.opacity = 0;
-  box4.style.opacity = 0;
-  info.innerHTML = "Image 2";
-  i = 1;
-};
- 
-a3.onclick = function () {
-  box1.style.opacity = 0;
-  box2.style.opacity = 0;
-  box3.style.opacity = 1;
-  box4.style.opacity = 0;
-  info.innerHTML = "Image 3";
-  i = 2;
-};
- 
-a4.onclick = function () {
-  box1.style.opacity = 0;
-  box2.style.opacity = 0;
-  box3.style.opacity = 0;
-  box4.style.opacity = 1;
-  info.innerHTML = "Image 4";
-  i = 3;
-};
- 
-document.addEventListener("keydown", (e) => {
-  if (e.keyCode == 37) {
-    right();
-  }
+var responsiveSlider = function() {
+
+var slider = document.getElementById("slider");
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById("slideWrap");
+var count = 1;
+var items = slideList.querySelectorAll("li").length;
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+window.addEventListener('resize', function() {
+  sliderWidth = slider.offsetWidth;
 });
- 
-document.addEventListener("keydown", (e) => {
-  if (e.keyCode == 39) {
-    right();
+
+
+var nextSlide = function() {
+  if(count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
   }
+  else if(count = items) {
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
+
+
+
+var prevSlide = function() {
+  if(count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
+
+
+next.addEventListener("click", function() {
+  nextSlide();
 });
- 
-function right() {
-  if (i == 0) {
-    box1.style.opacity = 1;
-    box2.style.opacity = 0;
-    box3.style.opacity = 0;
-    box4.style.opacity = 0;
-    info.innerHTML = "Image 1";
-    i++;
-  } else if (i == 1) {
-    box1.style.opacity = 0;
-    box2.style.opacity = 1;
-    box3.style.opacity = 0;
-    box4.style.opacity = 0;
-    info.innerHTML = "Image 2";
-    i++;
-  } else if (i == 2) {
-    box1.style.opacity = 0;
-    box2.style.opacity = 0;
-    box3.style.opacity = 1;
-    box4.style.opacity = 0;
-    info.innerHTML = "Image 3";
-    i++;
-  } else if (i == 3) {
-    box1.style.opacity = 0;
-    box2.style.opacity = 0;
-    box3.style.opacity = 0;
-    box4.style.opacity = 1;
-    info.innerHTML = "Image 4";
-    i = 0;
-  }
-}
- 
-function left() {
-  if (i == 0) {
-    box1.style.opacity = 0;
-    box2.style.opacity = 0;
-    box3.style.opacity = 0;
-    box4.style.opacity = 1;
-    info.innerHTML = "Image 1";
-    i++;
-  } else if (i == 1) {
-    box1.style.opacity = 0;
-    box2.style.opacity = 0;
-    box3.style.opacity = 1;
-    box4.style.opacity = 0;
-    info.innerHTML = "Image 2";
-    i++;
-  } else if (i == 2) {
-    box1.style.opacity = 0;
-    box2.style.opacity = 1;
-    box3.style.opacity = 0;
-    box4.style.opacity = 0;
-    info.innerHTML = "Image 3";
-    i++;
-  } else if (i == 3) {
-    box1.style.opacity = 1;
-    box2.style.opacity = 0;
-    box3.style.opacity = 0;
-    box4.style.opacity = 0;
-    info.innerHTML = "Image 4";
-    i = 0;
-  }
+
+
+prev.addEventListener("click", function() {
+  prevSlide();
+});
+
+setInterval(function() {
+  nextSlide()
+}, 10000);
+
+};
+
+window.onload = function() {
+responsiveSlider();  
 }
